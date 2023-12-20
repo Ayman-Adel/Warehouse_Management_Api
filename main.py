9a97d4d145961a26512c57e9b0a314584f7fe551
Serag el-dein Amged <71095930+SeragAmged@users.noreply.github.com>
2023-12-20 17:31:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -465,10 +465,6 @@ def update_comment(comment_id: int, comment: schemas.CommentCreate, db: Session 
 
 
 
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
 # auth
 # register
 # login
